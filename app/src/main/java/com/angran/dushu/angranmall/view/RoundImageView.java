df462a34507801b8d2f99955e6edee25c557d9ed
dushu <dushu@bytedance.com>
2019-03-02 16:12:01 +0800

    通用的titlebar

---
+++ app/src/main/java/com/angran/dushu/angranmall/view/RoundImageView.java
@@ -0,0 +1,8 @@
+package com.angran.dushu.angranmall.view;
+
+/**
+ * author: Create By dushu on 2019/3/2 16:09
+ * email : dushu@bytedance.com
+ */
+public class RoundImageView {
+}
---
+++ app/src/main/java/com/angran/dushu/angranmall/view/TitleBar/GeneralTitleBar.java
@@ -0,0 +1,9 @@
+package com.angran.dushu.angranmall.view.TitleBar;
+
+/**
+ * author: Create By dushu on 2019/3/2 16:08
+ * email : dushu@bytedance.com
+ * 通用titlebar
+ */
+public class GeneralTitleBar {
+}
