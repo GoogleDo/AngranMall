106bc5ad9f23beef6c504717ea589c249cb08443
dushu <dushu@bytedance.com>
2019-06-12 16:08:22 +0800

    全部货源 页面添加顶部条件筛选联动组件

---
+++ app/src/main/java/com/angran/dushu/angranmall/product/AllPreductActivity.java
@@ -0,0 +1,10 @@
+package com.angran.dushu.angranmall.product;
+
+import base.BaseActivity;
+
+/**
+ * author: Create By dushu on 2019/6/11 17:28
+ * email : dushu@bytedance.com
+ */
+public class AllPreductActivity extends BaseActivity {
+}
