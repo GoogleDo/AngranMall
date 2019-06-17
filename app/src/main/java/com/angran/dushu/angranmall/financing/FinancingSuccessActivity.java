72e684437de9b02e1eabb79eacf73956d61c3183
dushu <dushu@bytedance.com>
2019-06-17 15:48:13 +0800

    融资成功 页面

---
+++ app/src/main/java/com/angran/dushu/angranmall/financing/FinancingSuccessActivity.java
@@ -0,0 +1,10 @@
+package com.angran.dushu.angranmall.financing;
+
+import base.BaseActivity;
+
+/**
+ * author: Create By dushu on 2019/6/17 11:31
+ * email : dushu@bytedance.com
+ */
+public class FinancingSuccessActivity extends BaseActivity {
+}
